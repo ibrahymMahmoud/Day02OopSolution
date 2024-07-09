fc16efefe10ff077ce342149e64c7f07d09b38e6
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-07-09 18:29:05 +0300

    P04 Access Modifiers (Private Protected, Protected, Internal Protected)

---
+++ Common/TypeA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    internal class TypeA
+    {
+        private int x;              //access only in this scope of class
+        private protected int y;    //the class who inherit this class inherit y but private
+        protected int z;            //access only the class and driven class
+        internal protected int w;   //class who inherit this class inherit w internal if in the same file private if another file
+        internal int s;             //can access on the same file
+        public int b;               //can access on any files
+    }
+}
---
+++ Common/TypeB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    internal class TypeB : TypeA
+    {
+        TypeA TypeA= new TypeA();
+        public TypeB()
+        {
+            //x is private can't access
+            //y is private can't access,can access if  their inherit and same file
+            //z is private can't access,can access if  their inherit
+            //w is can access if in the same file,can access if  their inherit in same file (interinal) else (private)
+            //s is access only in the same file
+            //b is can access anywhere
+            
+        }
+    }
+}
