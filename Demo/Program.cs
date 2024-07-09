880a7a0392d00e9aaf8da0ad04c7f01157ae98e6
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-07-09 15:09:30 +0300

    P2 Class Vs Struct

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,18 +5,74 @@
         static void Main(string[] args)
         {
 
-         ///   Car car ;
-         ///   //Declare for reference of type "car" ,refering to null
-         ///   //CLR will allocate 4 bytes at stack for this reference "car"
-         ///   //clr will allocate 0 bytes at heap
-         ///   car = new Car(120 , 70 , "kia");
-         ///   //allocate required numbers of bytes in heap for the allocated object
-         ///   //initialize the allocated bytes of each and every attributes with the default value
-         ///   //call user-defined constructor [if exists]
-         ///   //assign the address of the allocated object in the heap to the referance
-         ///
-         ///   Console.WriteLine(car);
+            ///   Car car ;
+            ///   //Declare for reference of type "car" ,refering to null
+            ///   //CLR will allocate 4 bytes at stack for this reference "car"
+            ///   //clr will allocate 0 bytes at heap
+            ///   car = new Car(120 , 70 , "kia");
+            ///   //allocate required numbers of bytes in heap for the allocated object
+            ///   //initialize the allocated bytes of each and every attributes with the default value
+            ///   //call user-defined constructor [if exists]
+            ///   //assign the address of the allocated object in the heap to the referance
+            ///
+            ///   Console.WriteLine(car);
 
+            #region Class Vs struct
+            /*
+             In C#, both classes and structs are reference types, but they have some key differences:
+
+  1- Value vs. Reference Types:
+         *Classes
+            are reference types, meaning that when you create an instance of a class,
+            you are creating a reference to an object in memory. 
+            When you pass a class instance to a method or assign it to a variable, 
+            you are passing a reference to the same object.
+
+         *Structs
+            are value types, meaning that when you create an instance of a struct,
+            you are creating a copy of the data. When you pass a struct instance to a method or assign it to a variable,
+            you are passing a copy of the data.
+
+ 2- Memory Allocation:
+         *Classes 
+            are allocated on the managed heap, which is part of the .NET runtime's memory management system. 
+            This means that class instances can be garbage collected when they are no longer in use.
+
+        *Structs 
+            are allocated on the stack, which is a more efficient memory location for small, simple data structures. 
+            Struct instances are not subject to garbage collection.
+
+ 3- Inheritance:
+        *Classes 
+            support inheritance, which allows you to create derived classes 
+            that inherit properties and methods from a base class.
+
+       *Structs
+            do not support inheritance, but they can implement interfaces.
+
+ 4-Null Values:
+        *Classes 
+            can have null values, meaning that a variable of a class type can be assigned a null reference.
+
+        *Structs 
+            cannot have null values, as they are value types. 
+            However, you can use the Nullable<T> type to create a struct that can have a null value.
+
+ 5-Performance:
+        *Structs 
+            are generally more efficient than classes for small, 
+            simple data structures because they are allocated on the stack and do not require the overhead of the managed heap and garbage collection.
+        *Classes 
+            are better suited for more complex data structures and scenarios where reference semantics are important, 
+             such as when you need to pass objects by reference or use polymorphism.
+             
+             */
+
+            //The choice between using a class or a struct depends on the specific requirements of your application,
+            //such as the size and complexity of the data,
+            //the need for reference semantics,
+            //and performance considerations.
+            #endregion
         }
     }
 }
