945cdff7acd247eaaa6505aad781715a92b3e6ca
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-07-09 10:20:00 +0300

    P1 Class

---
+++ Demo/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Car
+    {
+		private int id;
+
+		private double speed;
+
+		private string? name;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+        public double Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public string? Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        //if no user-defined constructor exists, compiler will always generate empty parameterless constructor
+        //if you define a constructor, compiler will no longer generate empty parameterless constructor
+
+        public Car()
+        {
+            Console.WriteLine("empty constructor");
+        }
+        public Car( int id , double speed , string name):this(5,150)
+        {
+            Console.WriteLine("pramterise constructor");
+            this.id = id;
+            this.speed = speed; 
+            this.name = name;
+        }
+        public Car(int id , double speed)
+            //constructor chaining
+        {
+            this.id = id;
+            this.speed = speed;
+            this.name = "BMW";
+            Console.WriteLine("second constructor");
+        }
+        // constructor overloading
+        public override string ToString()
+        {
+            return $"Id = {id}\nSpeed = {speed}\nName = {name}";
+        }
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+         ///   Car car ;
+         ///   //Declare for reference of type "car" ,refering to null
+         ///   //CLR will allocate 4 bytes at stack for this reference "car"
+         ///   //clr will allocate 0 bytes at heap
+         ///   car = new Car(120 , 70 , "kia");
+         ///   //allocate required numbers of bytes in heap for the allocated object
+         ///   //initialize the allocated bytes of each and every attributes with the default value
+         ///   //call user-defined constructor [if exists]
+         ///   //assign the address of the allocated object in the heap to the referance
+         ///
+         ///   Console.WriteLine(car);
+
         }
     }
 }
