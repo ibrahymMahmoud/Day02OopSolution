45109066ac5b0c04670a802d415242f87f32acf2
ibrahymMahmoud <ibrahymmahmoud123@gmail.com>
2024-07-10 07:33:05 +0300

    Q2 Develop a Class to represent the Hiring Date Data:

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -48,6 +48,40 @@ namespace Assignment
     }
 
 
+    /*Part02 Q2*/
+    class HireDate 
+    {
+        private int day;
+        private int month;
+        private int year;
+
+        public HireDate(int day , int month , int year)
+        {
+            if (day < 1 || day > 31)
+                throw new ArgumentException("error,invaild day");
+            else 
+                this.day = day;
+
+            if (month < 1 || month > 12)
+                throw new ArgumentException("error,invaild month");
+            else
+                this.month = month;
+
+            if (year < 1900 || year > 2025)
+                throw new ArgumentException("error,invaild year");
+            else
+                this.year = year;
+
+        }
+
+        public override string ToString()
+        {
+            return $"date: {day}\\{month}\\{year}";
+        }
+
+    }
+
+
 
 
     internal class Program
@@ -57,18 +91,25 @@ namespace Assignment
             /*Part 02*/
             #region Q1
 
-           /// Employee Emp1 = new Employee(
-           ///     1,
-           ///     "Hema",
-           ///     SecurityLevel.Developer,
-           ///     Gender.Male,
-           ///     21_000,
-           ///     new DateTime(2024,12,24)
-           ///     );
-           /// Console.WriteLine( Emp1.ToString());
-           
+            /// Employee Emp1 = new Employee(
+            ///     1,
+            ///     "Hema",
+            ///     SecurityLevel.Developer,
+            ///     Gender.Male,
+            ///     21_000,
+            ///     new DateTime(2024,12,24)
+            ///     );
+            /// Console.WriteLine( Emp1.ToString());
+
 
             #endregion
+
+            #region Q2
+          ///  HireDate date = new HireDate(24, 12, 2001);
+          ///  Console.WriteLine(date); 
+            #endregion
+
+
         }
     }
 }
